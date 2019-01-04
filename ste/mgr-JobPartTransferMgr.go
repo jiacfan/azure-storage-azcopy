1bb02e7dea121b487ec6d505282a48451610c591
FAREAST\jiacfan <jiacfan@microsoft.com>
2019-01-04 17:48:19 +0800

    Redact secrets in logging.

--- ste/mgr-JobPartTransferMgr.go
+++ ste/mgr-JobPartTransferMgr.go
@@ -380,12 +380,9 @@ const (
 )
 
 func (jptm *jobPartTransferMgr) logTransferError(errorCode transferErrorCode, source, destination, errorMsg string, status int) {
-	msg := fmt.Sprintf("%v: ", errorCode) + common.URLStringExtension(source).RedactSigQueryParamForLogging() +
-		fmt.Sprintf(" : %03d : %s\n   Dst: ", status, errorMsg) + common.URLStringExtension(destination).RedactSigQueryParamForLogging()
+	msg := fmt.Sprintf("%v: ", errorCode) + common.URLStringExtension(source).RedactSecretQueryParamForLogging() +
+		fmt.Sprintf(" : %03d : %s\n   Dst: ", status, errorMsg) + common.URLStringExtension(destination).RedactSecretQueryParamForLogging()
 	jptm.Log(pipeline.LogError, msg)
-	//jptm.Log(pipeline.LogError, fmt.Sprintf("%v: %s: %03d : %s\n   Dst: %s",
-	//	errorCode, common.URLStringExtension(source).RedactSigQueryParamForLogging(),
-	//	status, errorMsg, common.URLStringExtension(destination).RedactSigQueryParamForLogging()))
 }
 
 func (jptm *jobPartTransferMgr) LogUploadError(source, destination, errorMsg string, status int) {
@@ -403,14 +400,14 @@ func (jptm *jobPartTransferMgr) LogS2SCopyError(source, destination, errorMsg st
 func (jptm *jobPartTransferMgr) LogError(resource, context string, err error) {
 	status, msg := ErrorEx{err}.ErrorCodeAndString()
 	jptm.Log(pipeline.LogError,
-		fmt.Sprintf("%s: %d: %s-%s", common.URLStringExtension(resource).RedactSigQueryParamForLogging(), status, context, msg))
+		fmt.Sprintf("%s: %d: %s-%s", common.URLStringExtension(resource).RedactSecretQueryParamForLogging(), status, context, msg))
 }
 
 func (jptm *jobPartTransferMgr) LogTransferStart(source, destination, description string) {
 	jptm.Log(pipeline.LogInfo,
 		fmt.Sprintf("Starting transfer: Source %q Destination %q. %s",
-			common.URLStringExtension(source).RedactSigQueryParamForLogging(),
-			common.URLStringExtension(destination).RedactSigQueryParamForLogging(),
+			common.URLStringExtension(source).RedactSecretQueryParamForLogging(),
+			common.URLStringExtension(destination).RedactSecretQueryParamForLogging(),
 			description))
 }
 
